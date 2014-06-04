413570cf0c78185790ca1ada22890ccbc7f757bc
Josh Brown <josh@roadfiresoftware.com>
2014-06-03 20:44:24 -0400

    Initial Commit

---
+++ SwiftFonts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftFonts
+//
+//  Created by Josh Brown on 6/3/14.
+//  Copyright (c) 2014 Roadfire Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
